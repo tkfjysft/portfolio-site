4ed8d2f16ff5898465c4ab77137114753a7541c2
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-06-06 08:40:28 +0900

    feat:認証ミドルウェアを追加5

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // これを追加
+//   output: 'export', // これを追加
   images: {
     unoptimized: true, // これを追加（画像をサーバーなしで表示するため）
   },
