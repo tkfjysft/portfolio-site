0e02e99fccf52fedaf5f4fa43e82225f7c90cc19
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-06-06 22:12:49 +0900

    fix:氏名をイニシャルに変更う

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 // src/config/site.ts
 
 export const siteConfig = {
-name: "RYOTA KONDO",
+name: "K.",
   role: "DTP Designer / Operator",
   description: "これまでの実務案件につきましては守秘義務の関係上、公開を控えさせていただいております。本サイトでは、実務と同等の要件・ターゲットを想定し、新たに制作したデザインサンプルを掲載しております。構成からデザインまで、実務を強く意識したクオリティを追求しておりますので、ぜひご高覧いただけますと幸いです。",
   copyright: "© 2026 RYOTA KONDO All Rights Reserved.",
